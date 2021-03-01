ea734acba9418c01a8c0c93480b9a3b6dc5e7b27
Leonardo <leonardo.bongianni@gmail.com>
2021-03-01 16:20:53 +0100

    Bozza js

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Chiedi eta
+var eta = parseInt(prompt("Quanti anni ha il passeggero?"));
+
+console.log(eta);
+
+// Chiedi distanza
+var km = parseFloat(prompt("Quanti chilometri deve percorrere?"));
+
+console.log(km);
+
+// calcolo prezzo
+var prezzo = km * 0.21;
+
+console.log(prezzo);
+
+var sconto18 = prezzo * 20 / 100;
+
+var sconto65 = prezzo * 40 / 100;
+
+if (eta < 18) {
+  alert("Il prezzo del suo biglietto è:" + (prezzo - sconto18).toFixed(2) + "€");
+  console.log("Il prezzo del suo biglietto è:" + (prezzo - sconto18).toFixed(2) + "€");
+} else if (eta >= 65) {
+  alert("Il prezzo del suo biglietto è:" + (prezzo - sconto65).toFixed(2) + "€");
+  console.log("Il prezzo del suo biglietto è:" + (prezzo - sconto65).toFixed(2) + "€");
+} else {
+  prezzo;
+}
+
+console.log();
