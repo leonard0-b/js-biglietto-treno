949f9a7d6086b5e5930698d548ec2ef94b04bb0e
Leonardo <leonardo.bongianni@gmail.com>
2021-03-01 18:37:18 +0100

    Esercizio Completo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,14 @@ var eta = parseInt(prompt("Quanti anni ha il passeggero?"));
 
 console.log(eta);
 
-document.getElementById('eta').innerHTML = "Il passeggero ha" + " " + eta + "anni";
+document.getElementById('eta').innerHTML = eta + " " + "anni";
 
 // Chiedi distanza
 var km = parseFloat(prompt("Quanti chilometri deve percorrere?"));
 
 console.log(km);
 
-document.getElementById('km').innerHTML = "Il passeggero deve percorrere" + " " + km + " " + "km";
+document.getElementById('km').innerHTML = km + " " + "km";
 
 // calcolo prezzo
 var prezzo = km * 0.21;
@@ -24,15 +24,15 @@ var scontoU18 = prezzo * 20 / 100;
 var scontoO65 = prezzo * 40 / 100;
 
 if (eta < 18) {
-  prezzoFinale = (prezzo - scontoU18).toFixed(2) + "€";
-  console.log((prezzo - scontoU18).toFixed(2) + "€");
+  prezzoFinale = (prezzo - scontoU18).toFixed(2) + " " + "€";
+  console.log((prezzo - scontoU18).toFixed(2) + " " + "€");
 } else if (eta >= 65) {
-  prezzoFinale = (prezzo - sconto65).toFixed(2) + "€";
-  console.log((prezzo - scontoO65).toFixed(2) + "€");
+  prezzoFinale = (prezzo - sconto65).toFixed(2) + " " + "€";
+  console.log((prezzo - scontoO65).toFixed(2) + " " + "€");
 } else {
-  prezzoFinale = prezzo - 0 + "€";
+  prezzoFinale = prezzo - 0 + " " + "€";
 }
 
 console.log();
 
-document.getElementById('price').innerHTML = "Il tuo prezzo è" + " " + prezzoFinale;
+document.getElementById('prezzoFinale').innerHTML = prezzoFinale;
